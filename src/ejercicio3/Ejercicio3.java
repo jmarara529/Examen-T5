03d6ef72e83e8f89fec609e7aa4c5533b559b8f7
José Martín Arance <jmarara529@g.educaand.es>
2024-02-27 09:03:17 +0100

    ejercicio3

---
+++ src/ejercicio3/Ejercicio3.java
@@ -0,0 +1,23 @@
+package ejercicio3;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    private static final Scanner entrada = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        List porducto = new ArrayList<>();
+
+        String nombreProducto = entrada.nextLine();
+
+
+
+        while (nombreProducto == "0"){
+
+        }
+
+    }
+}
